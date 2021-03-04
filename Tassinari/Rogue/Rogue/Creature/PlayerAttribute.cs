ceb6b94e8bdf20f593493f3dcab09ecc423e58c6
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-03-04 17:28:58 +0100

    Added PlayerAtribute enum

---
+++ Tassinari/Rogue/Rogue/Creature/PlayerAttribute.cs
@@ -0,0 +1,13 @@
+namespace Rogue.Creature
+{
+    public enum PlayerAttribute
+    {
+        MaxHp,
+        Hp,
+        Coins,
+        Level,
+        Strength,
+        Experience,
+        Food
+    }
+}
