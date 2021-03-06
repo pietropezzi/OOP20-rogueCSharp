c4d77dd8a5a2a89220274f55f5a2d51081045024
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-03-06 16:36:59 +0100

    Added creature and player interfaces

---
+++ Tassinari/Rogue/Rogue/Creature/Player.cs
@@ -0,0 +1,10 @@
+namespace Rogue.Creature
+{
+    /// <summary>
+    /// An interface modeling the player.
+    /// </summary>
+    public interface Player : ICreature<IPlayerLife>
+    {
+        
+    }
+}
