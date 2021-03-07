fecc5fe28f31373409c221c849c4fbd0388a428a
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-03-07 21:47:21 +0100

    Added strategy to increase level

---
+++ Tassinari/Rogue/Rogue/Creature/ILevelIncreaseStrategy.cs
@@ -0,0 +1,15 @@
+namespace Rogue.Creature
+{
+    /// <summary>
+    /// This interface manages the strategy with which to increase the player's level.
+    /// </summary>
+    public interface ILevelIncreaseStrategy
+    {
+        /// <summary>
+        /// Gets the corresponding level to the given experience.
+        /// </summary>
+        /// <param name="experience">the current experience value</param>
+        /// <returns>an <see cref="int"/> describing the level</returns>
+        int Level(int experience);
+    }
+}
---
+++ Tassinari/Rogue/Rogue/Creature/StandardLevelIncreaseStrategy.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+
+namespace Rogue.Creature
+{
+    /// <summary>
+    /// This class represents the standard strategy with which increase the level value.
+    /// </summary>
+    public class StandardLevelIncreaseStrategy : ILevelIncreaseStrategy
+    {
+        /// <summary>
+        /// An enumeration represents the level with the corresponding experience value.
+        /// </summary>
+        private enum ExpLevel
+        {
+            L1 = 0,     L2 = 7,     L3 = 16,    L4 = 30, 
+            L5 = 50,    L6 = 75,    L7 = 115,   L8 = 170, 
+            L9 = 250,   L10 = 355,  L11 = 525,  L12 = 725,
+            L13 = 1050, L14 = 1475, L15 = 2125, L16 = 3000,
+            L17 = 4250, L18 = 6000, L19 = 8500, L20 = 12_000
+        }
+        
+        private readonly List<Tuple<int, int>> values;
+        
+        /// <inheritdoc cref="ILevelIncreaseStrategy"/>
+        public int Level(int experience)
+        {
+            var l = Enum.GetValues(typeof(ExpLevel))
+                .Cast<int>()
+                .Where(i => i <= experience)
+                .ToList();
+            return l.Count;
+        }
+
+    }
+}
