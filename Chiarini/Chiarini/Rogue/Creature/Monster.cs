85fa91fa2a32059083498f7c8bbafafcaa37d9f4
Fabio Chiarini <fabio.chiarini@studio.unibo.it>
2021-03-08 22:38:17 +0100

    add Chiarini task

---
+++ Chiarini/Chiarini/Rogue/Creature/Monster.cs
@@ -0,0 +1,132 @@
+﻿using System;
+namespace Rogue.Creature
+{
+	 /// <summary>
+	 /// Class representing a game <see cref="IMonster"/> 
+	 /// </summary> 
+	public class Monster : IMonster
+	{
+       
+
+	  	private readonly MonsterType name;
+	    private readonly MonsterLife life;
+	    private readonly int AC;
+    	private readonly int money;
+    	private readonly int itemChange;
+    	// private readonly Potion item;
+    	private readonly Special special;
+    	private readonly Pair<int, int> damage;
+
+
+    	public Monster(MonsterType name)
+    	{
+    	    this.name = name;
+    		Random rnd = new Random();
+    		if(this.name.Equals(MonsterType.AIR_ELEMENTAL)) {
+    		   this.life = new MonsterLife(rnd.Next(4, 33-4), 20);
+    	       this.AC = 2;
+    	       this.money = 0;
+    	       this.itemChange = 20;
+    	       this.special = new Special(true, true, false, false, false, false);
+    	       this.damage = new Pair<int, int>(4, 24);
+    	    }
+    	
+    		else if(this.name.Equals(MonsterType.BAT)) {
+    		   this.life = new MonsterLife(rnd.Next(1, 8-1), 1);
+    	       this.AC = 3;
+    	       this.money = 0;
+    	       this.itemChange = 0;
+    	       this.special = new Special(false, true, false, true, false, false);
+    	       this.damage = new Pair<int, int>(1, 2);
+    	    }
+    	
+    	   else if(this.name.Equals(MonsterType.CENTAUR)) {
+    		   this.life = new MonsterLife(rnd.Next(4, 32-4), 17);
+    	       this.AC = 4;
+    	       this.money = 51;
+    	       this.itemChange = 10;
+    	       this.special = new Special();
+    	       this.damage = new Pair<int, int>(4, 24);
+    	    }
+    	}
+    
+    	/// <summary>
+    	/// return the type of the monster
+    	/// </summary>
+    	/// <returns>the type of the monster</returns>    
+    	public MonsterType getMonsterType() {
+    		return this.name;
+    	}
+    
+    	/// <summary>
+    	/// return the monster's life  
+    	/// </summary>
+    	/// <returns>the monster life </returns>
+    	public MonsterLife getMonsterLife() {
+    		return this.life;
+    	}
+    
+    	/// <summary>
+    	/// return the Armor Class of the monster
+    	/// </summary>
+    	/// <returns>the Armor Class of the monster </returns>
+    	public int getAC() {
+    		return this.AC;
+    	}
+    
+    	/// <summary>
+    	/// return the maximum damage the monster can do the minimum is 1 
+    	/// </summary>
+    	/// <returns>the maximum damage the monster can do the minimum is 1 </returns>
+    	public Pair<int, int> getDamage() {
+    		return this.damage;
+    	}
+
+    	/// <summary>
+    	/// return the monster's special skill
+    	/// </summary>
+    	/// <returns>the monster's special skill</returns>
+    	public Special getSpecial() {
+    		return this.special;
+    	}
+
+    	/// <summary>
+    	/// return the amount of money of the monster 
+    	/// </summary>
+    	/// <returns>the amount of money of the monster </returns>
+    	public int getMoney() {
+    		return this.money;
+    	}
+
+    	/// <summary>
+    	/// return the item's possibility to drop
+    	/// </summary>
+    	/// <returns>the item's possibility to drop</returns>
+    	public int getItemChange() {
+    		return this.itemChange;
+    	}
+
+    	/// <summary>
+    	/// return the damage that the monster does for that single attack 
+    	/// </summary>
+    	/// <returns>the damage that the monster does for that single attack </returns>
+    	public int attackDamage() {
+    		Random rnd = new Random();
+    		return rnd.Next(this.getDamage().GetFirst(), this.getDamage().GetSecond());
+    	}
+    	
+    	/* public Potion getItem() {
+    	 *     return this.item;
+    	 * }
+    	 */
+
+    	/// <summary>
+    	/// return a random number of the item drop
+    	/// </summary>
+    	/// <returns>a random number of the item drop</returns>
+    	public int dropItem() {
+    		Random rnd = new Random();
+    		return rnd.Next(100);
+    	}   
+	}
+}
---
+++ Chiarini/Chiarini/Rogue/Creature/Test.cs
@@ -0,0 +1,234 @@
+﻿using System;
+
+namespace Rogue.Creature
+{
+	class Test
+	{
+		public static void Main(string[] args)
+		{			
+			Monster air = new Monster(MonsterType.AIR_ELEMENTAL);
+
+			Console.WriteLine("Name " + air.getMonsterType().ToString());
+
+			if(air.getSpecial().isHostile()){
+				Console.WriteLine("isHostile");
+			}
+
+			if(air.getSpecial().isFlying()){
+				Console.WriteLine("isFlying");
+			}
+
+			if(air.getSpecial().isDrainLife()){
+				Console.WriteLine("isDrainLife");
+			}
+
+			if(air.getSpecial().isFlyingRandom()){
+				Console.WriteLine("isFlyingRandom");
+			}
+
+			if(air.getSpecial().isGreedy()){
+				Console.WriteLine("isGreedy");
+			}
+
+			if(air.getSpecial().isPoisonous()){
+				Console.WriteLine("isPoisonous");
+			}
+
+			Console.WriteLine("AC " + air.getAC());
+			Console.WriteLine("Damage " + air.getDamage().ToString());
+			Console.WriteLine("ItemChange " + air.getItemChange());
+			Console.WriteLine("Money " + air.getMoney());
+			Console.WriteLine("Attack " + air.attackDamage());
+			Console.WriteLine("DropItem " + air.dropItem());
+			Console.WriteLine("HP " + air.getMonsterLife().getHealthPoints());
+			Console.WriteLine("EXP " + air.getMonsterLife().getExperience() + "\n");
+
+			Monster bat = new Monster(MonsterType.BAT);
+
+			Console.WriteLine("Name " + bat.getMonsterType().ToString());
+
+			if(bat.getSpecial().isHostile()){
+				Console.WriteLine("isHostile");
+			}
+
+			if(bat.getSpecial().isFlying()){
+				Console.WriteLine("isFlying");
+			}
+
+			if(bat.getSpecial().isDrainLife()){
+				Console.WriteLine("isDrainLife");
+			}
+
+			if(bat.getSpecial().isFlyingRandom()){
+				Console.WriteLine("isFlyingRandom");
+			}
+
+			if(bat.getSpecial().isGreedy()){
+				Console.WriteLine("isGreedy");
+			}
+
+			if(bat.getSpecial().isPoisonous()){
+				Console.WriteLine("isPoisonous");
+			}
+
+			Console.WriteLine("AC " + bat.getAC());
+			Console.WriteLine("Damage " + bat.getDamage().ToString());
+			Console.WriteLine("ItemChange " + bat.getItemChange());
+			Console.WriteLine("Money " + bat.getMoney());
+			Console.WriteLine("Attack " + bat.attackDamage());
+			Console.WriteLine("DropItem " + bat.dropItem());
+			Console.WriteLine("HP " + bat.getMonsterLife().getHealthPoints());
+			Console.WriteLine("EXP " + bat.getMonsterLife().getExperience() + "\n");
+
+
+			Monster centaur = new Monster(MonsterType.CENTAUR);
+
+			Console.WriteLine("Name " + centaur.getMonsterType().ToString());
+
+			if(centaur.getSpecial().isHostile()){
+				Console.WriteLine("isHostile");
+			}
+
+			if(centaur.getSpecial().isFlying()){
+				Console.WriteLine("isFlying");
+			}
+
+			if(centaur.getSpecial().isDrainLife()){
+				Console.WriteLine("isDrainLife");
+			}
+
+			if(centaur.getSpecial().isFlyingRandom()){
+				Console.WriteLine("isFlyingRandom");
+			}
+
+			if(centaur.getSpecial().isGreedy()){
+				Console.WriteLine("isGreedy");
+			}
+
+			if(centaur.getSpecial().isPoisonous()){
+				Console.WriteLine("isPoisonous");
+			}
+
+			Console.WriteLine("AC " + centaur.getAC());
+			Console.WriteLine("Damage " + centaur.getDamage().ToString());
+			Console.WriteLine("ItemChange " + centaur.getItemChange());
+			Console.WriteLine("Money " + centaur.getMoney());
+			Console.WriteLine("Attack " + centaur.attackDamage());
+			Console.WriteLine("DropItem " + centaur.dropItem());
+			Console.WriteLine("HP " + centaur.getMonsterLife().getHealthPoints());
+			Console.WriteLine("EXP " + centaur.getMonsterLife().getExperience() + "\n");
+
+			// TODO: Implement Functionality Here
+
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+} 
+/*using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rogue.Creature
+{
+	
+    [TestClass]
+	class Test
+	{
+		private static readonly int IPOTETICAL_DAMAGE = 999;
+
+        private static readonly int AIR_ELEMENTAL_EXPERIENCE = 20;
+        private static readonly int AIR_ELEMENTAL_MONEY = 0;
+        private static readonly int AIR_ELEMENTAL_ITEM_CHANGE = 20;
+   	    private static readonly int AIR_ELEMENTAL_HP_MIN = 4;
+   		private static readonly int AIR_ELEMENTAL_HP_MAX = 32;
+        private static readonly int AIR_ELEMENTAL_DAMAGE_MIN = 4;
+        private static readonly int AIR_ELEMENTAL_DAMAGE_MAX = 24;
+        private static readonly int AIR_ELEMENTAL_AC = 2;
+        
+        private static readonly int BAT_EXPERIENCE = 1;
+        private static readonly int BAT_MONEY = 0;
+        private static readonly int BAT_ITEM_CHANGE = 0;
+        private static readonly int BAT_HP_MIN = 1;
+        private static readonly int BAT_HP_MAX = 8;
+        private static readonly int BAT_DAMAGE_MIN = 1;
+        private static readonly int BAT_DAMAGE_MAX = 2;
+        private static readonly int BAT_AC = 3;
+        
+        private static readonly int CENTAUR_EXPERIENCE = 17;
+        private static readonly int CENTAUR_MONEY = 51;
+        private static readonly int CENTAUR_ITEM_CHANGE = 10;
+        private static readonly int CENTAUR_HP_MIN = 4;
+        private static readonly int CENTAUR_HP_MAX = 32;
+        private static readonly int CENTAUR_DAMAGE_MIN = 4;
+        private static readonly int CENTAUR_DAMAGE_MAX = 32;
+        private static readonly int CENTAUR_AC = 4;
+            
+		[TestMethod]
+        public void testAirElemental() {
+       	
+	        Monster mon = new Monster(MonsterType.AIR_ELEMENTAL);
+
+	        assertEquals(AIR_ELEMENTAL_EXPERIENCE, mon.getMonsterLife().getExperience());
+	        assertEquals(AIR_ELEMENTAL_MONEY, mon.getMoney());
+	        assertEquals(AIR_ELEMENTAL_ITEM_CHANGE, mon.getItemChange());
+	        assertTrue(AIR_ELEMENTAL_HP_MIN <= mon.getMonsterLife().getHealthPoints() && mon.getMonsterLife().getHealthPoints() <= AIR_ELEMENTAL_HP_MAX);
+	        assertTrue(AIR_ELEMENTAL_DAMAGE_MIN <= mon.attackDamage() && mon.attackDamage() <= AIR_ELEMENTAL_DAMAGE_MAX);
+	        assertEquals(AIR_ELEMENTAL_AC, mon.getAC());
+
+	        assertFalse(mon.getSpecial().isDrainLife());
+	        assertFalse(mon.getSpecial().isFlyingRandom());
+	        assertTrue(mon.getSpecial().isFlying());
+	        assertFalse(mon.getSpecial().isGreedy());
+	        assertTrue(mon.getSpecial().isHostile());
+	        assertFalse(mon.getSpecial().isPoisonous());
+	
+	        mon.getMonsterLife().hurt(IPOTETICAL_DAMAGE);
+	        assertTrue(mon.getMonsterLife().isDead());
+        }
+		
+		[TestMethod]
+    	public void testBat() {
+        
+       	    Monster mon = new Monster(MonsterType.BAT);
+
+            assertEquals(BAT_EXPERIENCE, mon.getMonsterLife().getExperience());
+	        assertEquals(BAT_MONEY, mon.getMoney());
+	        assertEquals(BAT_ITEM_CHANGE, mon.getItemChange());
+    	    assertTrue(BAT_HP_MIN <= mon.getMonsterLife().getHealthPoints() && mon.getMonsterLife().getHealthPoints() <= BAT_HP_MAX);
+    	    assertTrue(BAT_DAMAGE_MIN <= mon.attackDamage() && mon.attackDamage() <= BAT_DAMAGE_MAX);
+    	    assertEquals(BAT_AC, mon.getAC());
+	
+    	    assertFalse(mon.getSpecial().isDrainLife());
+    	    assertTrue(mon.getSpecial().isFlyingRandom());
+    	    assertTrue(mon.getSpecial().isFlying());
+    	    assertFalse(mon.getSpecial().isGreedy());
+    	    assertFalse(mon.getSpecial().isHostile());
+    	    assertFalse(mon.getSpecial().isPoisonous());
+
+    	    mon.getMonsterLife().hurt(IPOTETICAL_DAMAGE);
+    	    assertTrue(mon.getMonsterLife().isDead());
+    	}
+		 
+		[TestMethod]
+    	public void testCentaur() {
+	       
+    	    Monster mon = new Monster(MonsterType.CENTAUR);
+
+    	    assertEquals(CENTAUR_EXPERIENCE, mon.getMonsterLife().getExperience());
+    	    assertEquals(CENTAUR_MONEY, mon.getMoney());
+    	    assertEquals(CENTAUR_ITEM_CHANGE, mon.getItemChange());
+    	    assertTrue(CENTAUR_HP_MIN <= mon.getMonsterLife().getHealthPoints() && mon.getMonsterLife().getHealthPoints() <= CENTAUR_HP_MAX);
+    	    assertTrue(CENTAUR_DAMAGE_MIN <= mon.attackDamage() && mon.attackDamage() <= CENTAUR_DAMAGE_MAX);
+    	    assertEquals(CENTAUR_AC, mon.getAC());
+
+    	    assertFalse(mon.getSpecial().isDrainLife());
+    	    assertFalse(mon.getSpecial().isFlyingRandom());
+    	    assertFalse(mon.getSpecial().isFlying());
+    	    assertFalse(mon.getSpecial().isGreedy());
+    	    assertFalse(mon.getSpecial().isHostile());
+    	    assertFalse(mon.getSpecial().isPoisonous());
+
+    	    mon.getMonsterLife().hurt(IPOTETICAL_DAMAGE);
+    	    assertTrue(mon.getMonsterLife().isDead());
+    	}
+	}
+}*/
