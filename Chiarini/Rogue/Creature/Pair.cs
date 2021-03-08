b0a7bed73aa2fa8bd2c88ef73a0fd392ca2038f5
Fabio Chiarini <fabio.chiarini@studio.unibo.it>
2021-03-08 22:11:42 +0100

    Update Pair.cs

--- Chiarini/Rogue/Creature/Pair.cs
+++ Chiarini/Rogue/Creature/Pair.cs
@@ -6,27 +6,27 @@
 	/// </summary>
 	public class Pair<TFirst, Tsecond> { 
 
-   		private TFirst first;
-   		private Tsecond second;
+   	    private TFirst first;
+   	    private Tsecond second;
 
-	  	 /// <summary>
-	   	/// The class constructor.
+	    /// <summary>
+	    /// The class constructor.
 	    /// </summary>
- 	   /// <param name="first">the first value</param>
+ 	    /// <param name="first">the first value</param>
 	    /// <param name="second">the second value</param>
 	    public Pair(TFirst first, Tsecond second)
 	    {
- 	       this.first = first;
- 	       this.second = second;
- 	   }
+ 	          this.first = first;
+ 	          this.second = second;
+ 	    }
 
- 	   /// <summary>
- 	   /// return the first value.
- 	   /// </summary>
- 	   /// <returns>the first value.</returns>
- 	   public TFirst GetFirst() {
-        return this.first;
-  	   }
+ 	    /// <summary>
+ 	    /// return the first value.
+ 	    /// </summary>
+ 	    /// <returns>the first value.</returns>
+ 	    public TFirst GetFirst() {
+                return this.first;
+  	    }
 
 	    /// <summary>
 	    /// return the second value.
@@ -34,12 +34,12 @@
 	    /// <returns>the second value.</returns>
  	    public Tsecond GetSecond() {
        		return this.second;
-    	}
+    	    }
 
  	   /// <summary>
  	   /// return i to string of the pair.
-	    /// </summary>
-	    /// <returns>the string</returns>
+	   /// </summary>
+	   /// <returns>the string</returns>
  	   public override string ToString() {
  	       return "<" + first + "," + second + ">";
   	   }
